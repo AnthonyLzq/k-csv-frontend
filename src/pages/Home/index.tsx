110f376babdaf2b5eda3b63241aad2e28255883f
AnthonyLzq <sluzquinosa@uni.pe>
2021-11-10 01:44:38 -0500

    feat: implemented buttons to allow download and upload process
    TODO:
      - implement funcionality for upload button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,186 +1,74 @@
-import { FC, MouseEvent, useState } from 'react'
-import { Button, IconButton, InputAdornment, InputBase } from '@material-ui/core'
-import { CloudUpload, Visibility, VisibilityOff } from '@material-ui/icons'
-import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
-import { DropzoneArea } from 'material-ui-dropzone'
-import Swal from 'sweetalert2'
+import { FC, useState } from 'react'
+import { Button, Grid, ThemeOptions, ThemeProvider } from '@material-ui/core'
+import { CloudDownload, CloudUpload } from '@material-ui/icons'
+import { createTheme } from '@material-ui/core/styles'
 
-import { Loader, Header } from '../../components'
-import { callApi } from '../../utils'
 import './index.css'
+import { Body, Header } from '../../components'
 
-const theme = createTheme({
-  overrides: {
-    MuiDropzoneArea: {
-      root: {
-        backgroundColor: '#282c34',
-        borderColor    : '#d5d5d5',
-        color          : '#d5d5d5',
-        minHeight      : 50,
-        marginLeft     : 'auto',
-        marginRight    : 'auto',
-        width          : '50vw'
-      },
-      icon: {
-        color: '#d5d5d5'
-      }
-    },
-    MuiDropzonePreviewList: {
-      root: {
-        alignItems    : 'center',
-        color         : '#d5d5d5',
-        display       : 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        marginBottom  : 15
-      }
-    },
-    MuiChip: {
-      root: {
-        color: '#d5d5d5'
-      },
-      outlined: {
-        border: '1px solid #d5d5d5'
-      },
-      deleteIcon: {
-        color: '#d5d5d5'
-      }
+const themeOptions: ThemeOptions = {
+  breakpoints: {
+    values: {
+      xs : 0,
+      sm : 620,
+      md : 960,
+      lg : 1200,
+      xl : 1536,
+      xxl: 1800
     }
   }
-})
+}
+
+const theme = createTheme(themeOptions)
 
 const Home: FC = () => {
-  const [file, setFile] = useState<File | null>(null)
-  const [apiKey, setApiKey] = useState('')
-  const [showApiKey, setShowApiKey] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-
-  const onChange = (files: File[]) => {
-    if (files.length === 0) setFile(null)
-    else setFile(files[0])
-  }
-
-  const handleSubmit = async (
-    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) => {
-    e.preventDefault()
-
-    if (!file)
-      return Swal.fire({
-        background: '#d5d5d5',
-        icon      : 'error',
-        title     : 'Missing csv!'
-      })
-
-    if (!apiKey)
-    return Swal.fire({
-        background: '#d5d5d5',
-        icon      : 'error',
-        title     : 'Missing apiKey!'
-      })
-
-    setIsLoading(true)
-    const body = new FormData()
-    body.append('file', file as File)
-    const { response, error } = await callApi({
-      body,
-      headers: {
-        'api-key': apiKey
-      }
-    })
-    setIsLoading(false)
-
-    if (error)
-      Swal.fire({
-        background: '#d5d5d5',
-        icon      : 'error',
-        title     : error
-      })
-    else
-      Swal.fire({
-        background        : '#d5d5d5',
-        confirmButtonColor: '#5e7ce2',
-        icon              : 'success',
-        iconColor         : '#5e7ce2',
-        text              : response as string,
-        title             : 'Success!'
-      })
-  }
+  const [isUploadSelected, setIsUploadSelected] = useState(false)
 
   return (
-    <>
+    <div style={{ width: '70vw', margin: '0 auto', overflow: 'hidden' }}>
       <Header />
-      <section className='App-section'>
-        <form>
-          <MuiThemeProvider theme={theme}>
-            <DropzoneArea
-              showPreviewsInDropzone={true}
-              useChipsForPreview
-              acceptedFiles={['.csv', 'text/csv']}
-              dropzoneText='Drag and drop your csv here or click'
-              filesLimit={1}
-              maxFileSize={500_000_000}
-              onChange={onChange}
-              dropzoneProps={{
-                disabled: isLoading
-              }}
-              previewChipProps={{
-                disabled: isLoading
-              }}
-            />
-          </MuiThemeProvider>
-          <div className='form-control'>
-            <label htmlFor='apiKey'>apiKey: </label>
-            <InputBase
+      <h2 style={{ color: '#d5d5d5' }}>Please, select an option:</h2>
+      <ThemeProvider theme={theme}>
+        <Grid
+          container
+          className='buttons-container'
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+          spacing={2}
+        >
+          <Grid item xs={12} sm={6} md={5} lg={5} xl={4}>
+            <Button
+              startIcon={<CloudUpload />}
+              type='submit'
+              onClick={() => setIsUploadSelected(false)}
+              variant='contained'
+              size='large'
               style={{
-                background  : '#87898c',
-                color       : '#282c34',
-                borderColor : 'transparent',
-                borderRadius: '0.25rem',
-                marginLeft  : '1rem',
-                padding     : '0.25rem 0.5rem 0rem'
+                backgroundColor: '#5e7ce2'
               }}
-              required
-              type={showApiKey ? 'text' : 'password'}
-              id='apiKey'
-              name='apiKey'
-              value={apiKey}
-              onChange={e => setApiKey(e.target.value)}
-              endAdornment={
-                <InputAdornment position='end'>
-                  <IconButton
-                    onClick={() => setShowApiKey(!showApiKey)}
-                    onMouseDown={e => e.preventDefault()}
-                  >
-                    {showApiKey ? (
-                      <Visibility style={{ color: '#282c34' }} />
-                    ) : (
-                      <VisibilityOff style={{ color: '#282c34' }} />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              }
-              disabled={isLoading}
-            />
-          </div>
-          <Button
-            startIcon={<CloudUpload />}
-            type='submit'
-            onClick={handleSubmit}
-            disabled={isLoading}
-            variant='contained'
-            size='large'
-            style={{
-              backgroundColor: '#5e7ce2',
-              marginTop      : '1rem'
-            }}
-          >
-            Update csv
-          </Button>
-        </form>
-      </section>
-      {isLoading && <Loader />}
-    </>
+            >
+              Upload a new csv
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6} md={5} lg={5} xl={4}>
+            <Button
+              startIcon={<CloudDownload />}
+              type='submit'
+              onClick={() => setIsUploadSelected(false)}
+              variant='contained'
+              size='large'
+              style={{
+                backgroundColor: '#5e7ce2'
+              }}
+            >
+              Download last csv uploaded
+            </Button>
+          </Grid>
+        </Grid>
+      </ThemeProvider>
+      {/* <Body /> */}
+    </div>
   )
 }
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,41 @@
 import { StyleRules } from '@material-ui/core'
 
-// Interface would be maintained by the repository.
-// Of course, this could be split into separate types.
 interface MuiDropzoneNameToClassKey {
-  MuiDropzoneArea: 'root' | 'text' | 'active' | 'invalid' | 'textContainer' | 'icon'
+  MuiDropzoneArea:
+    | 'root'
+    | 'text'
+    | 'active'
+    | 'invalid'
+    | 'textContainer'
+    | 'icon'
   MuiDropzonePreviewList: 'root' | 'imageContainer' | 'image' | 'removeButton'
-  MuiDropzoneSnackbar: 'infoAlert' | 'successAlert' | 'warningAlert' | 'errorAlert' | 'message' | 'icon' | 'closeButton'
+  MuiDropzoneSnackbar:
+    | 'infoAlert'
+    | 'successAlert'
+    | 'warningAlert'
+    | 'errorAlert'
+    | 'message'
+    | 'icon'
+    | 'closeButton'
 }
 
-// Overrides would be maintained by the user.
-
-/**
- * Type which accepts anything that's declared above. Can be exported if you wish to define a constant using the types.
- */
 type OverridesNameToClassKey = {
-  // MaterialUI internally uses the following definition, which also works in my project.
-  [Name in keyof MuiDropzoneNameToClassKey]?: Partial<StyleRules<MuiDropzoneNameToClassKey[Name]>>
+  [Name in keyof MuiDropzoneNameToClassKey]?: Partial<
+    StyleRules<MuiDropzoneNameToClassKey[Name]>
+  >
 }
 
-/**
- * Redeclare the MUI theme override type to allow anything we've defined here to be allowed into the overrides list.
- */
 declare module '@material-ui/core/styles/overrides' {
   export interface ComponentNameToClassKey extends OverridesNameToClassKey {}
+}
+
+declare module '@material-ui/core/styles/createBreakpoints' {
+  interface BreakpointOverrides {
+    xs: true
+    sm: true
+    md: true
+    lg: true
+    xl: true
+    xxl: true
+  }
 }
