a0d01d156d091b8293d3cb113877d6b7d9bd661b
AnthonyLzq <sluzquinosa@uni.pe>
2021-12-12 15:18:44 -0500

    feat: implemented download button

--- src/components/GridContainer/index.tsx
+++ src/components/GridContainer/index.tsx
@@ -1,57 +1,57 @@
 import { FC } from 'react'
-import { Button, Grid, Tooltip } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import { CloudDownload, CloudUpload } from '@material-ui/icons'
 
 interface GridContainerProps {
-  setIsUploadSelected: (value: boolean) => void
+  setOption: (value: Option) => void
 }
 
-const GridContainer: FC<GridContainerProps> = ({ setIsUploadSelected }) => (
-  <Grid
-    container
-    direction='row'
-    justifyContent='center'
-    alignItems='center'
-    spacing={2}
-    style={{
-      margin  : '0 auto',
-      maxWidth: 960,
-      width   : '80%'
-    }}
-  >
-    <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-      <Button
-        startIcon={<CloudUpload />}
-        onClick={() => setIsUploadSelected(true)}
-        variant='contained'
-        size='large'
-        fullWidth
-        style={{
-          backgroundColor: '#5e7ce2'
-        }}
-      >
-        Upload a new csv
-      </Button>
+const GridContainer: FC<GridContainerProps> = props => {
+  const { setOption } = props
+
+  return (
+    <Grid
+      container
+      direction='row'
+      justifyContent='center'
+      alignItems='center'
+      spacing={2}
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        width: '80%'
+      }}
+    >
+      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+        <Button
+          onClick={() => setOption('upload')}
+          startIcon={<CloudUpload />}
+          variant='contained'
+          size='large'
+          fullWidth
+          style={{
+            backgroundColor: '#5e7ce2'
+          }}
+        >
+          Upload a new csv
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+        <Button
+          onClick={() => setOption('download')}
+          startIcon={<CloudDownload />}
+          variant='contained'
+          size='large'
+          fullWidth
+          style={{
+            backgroundColor: '#5e7ce2'
+          }}
+        >
+          Download last csv uploaded
+        </Button>
+      </Grid>
     </Grid>
-    <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-      <Tooltip title='Not available yet :(' arrow>
-        <div>
-          <Button
-            disabled
-            startIcon={<CloudDownload />}
-            variant='contained'
-            size='large'
-            fullWidth
-            style={{
-              backgroundColor: '#5e7ce2'
-            }}
-          >
-            Download last csv uploaded
-          </Button>
-        </div>
-      </Tooltip>
-    </Grid>
-  </Grid>
-)
+  )
+}
 
 export { GridContainer }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,21 +3,21 @@ import { FC, useState } from 'react'
 import { Body, Header, GridContainer } from '../../components'
 
 const Home: FC = () => {
-  const [isUploadSelected, setIsUploadSelected] = useState(false)
+  const [option, setOption] = useState<Option>(null)
 
   return (
     <div style={{ width: '100vw', margin: '0 auto', overflow: 'hidden' }}>
       <Header />
-      {isUploadSelected ? (
-        <Body setIsUploadSelected={setIsUploadSelected} />
-      ) : (
+      {!option ? (
         <>
           <h2 style={{ color: '#d5d5d5' }}>
             Welcome!
             <br /> Please, select an option:
           </h2>
-          <GridContainer setIsUploadSelected={setIsUploadSelected} />
+          <GridContainer setOption={setOption}/>
         </>
+      ) : (
+        <Body option={option} setOption={setOption} />
       )}
     </div>
   )
